2edc8bc6af9cfa36ab2302d77508420a09ccb80c
Sami CHNITER <sami.chniter@gmail.com>
2019-06-23 19:01:13 +0100

    csharp client.

---
+++ csharp/PlateRecognizer/PlateReaderResult.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PlateRecognizer
+{
+    [DataContract]
+    public class Box
+    {
+
+        [DataMember(Name = "xmin")]
+        public int Xmin { get; set; }
+
+        [DataMember(Name = "ymin")]
+        public int Ymin { get; set; }
+
+        [DataMember(Name = "ymax")]
+        public int Ymax { get; set; }
+
+        [DataMember(Name = "xmax")]
+        public int Xmax { get; set; }
+    }
+
+    [DataContract]
+    public class Result
+    {
+
+        [DataMember(Name = "box")]
+        public Box Box { get; set; }
+
+        [DataMember(Name = "plate")]
+        public string Plate { get; set; }
+
+        [DataMember(Name = "score")]
+        public double Score { get; set; }
+
+        [DataMember(Name = "dscore")]
+        public double Dscore { get; set; }
+    }
+
+    [DataContract]
+    public class PlateReaderResult
+    {
+
+        [DataMember(Name = "processing_time")]
+        public double ProcessingTime { get; set; }
+
+        [DataMember(Name = "version")]
+        public int Version { get; set; }
+
+        [DataMember(Name = "results")]
+        public IList<Result> Results { get; set; }
+
+        [DataMember(Name = "filename")]
+        public string Filename { get; set; }
+    }
+
+
+}
