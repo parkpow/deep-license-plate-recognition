be768b640b8702726ce3c2c8289d5c441feb4e61
Muhia John NJoroge <clbnjoroge@gmail.com>
2019-07-27 12:15:35 +0300

    add a list of regions

--- plate_recognition.py
+++ plate_recognition.py
@@ -15,11 +15,11 @@ def parse_arguments():
         description=
         'Read license plates from images and output the result as JSON.',
         epilog=
-        'For example: python plate_recognition.py --api MY_API_KEY "/path/to/vehicle-*.jpg"'
+        'For example: python plate_recognition.py --api-key MY_API_KEY "/path/to/vehicle-*.jpg"'
     )
-    parser.add_argument('--api', help='Your API key.', required=True)
-    parser.add_argument('--region', help='Match the license plate pattern fo specific region', required=False)
-    parser.add_argument('--url', default='http://localhost:8080', help="Url to self hosted sdk", required=False)
+    parser.add_argument('--api', '--api-key', help='Your API key.', required=False)
+    parser.add_argument('--regions', help='Match the license plate pattern fo specific region', required=False, action="append")
+    parser.add_argument('--url','--sdk-url', default='http://localhost:8080', help="Url to self hosted sdk", required=False)
     parser.add_argument('files', nargs='+', help='Path to vehicle images')
     return parser.parse_args()
 
@@ -28,19 +28,22 @@ def main():
     args = parse_arguments()
     result = []
     paths = args.files
-    region = args.region
+    regions = args.regions
+
+    if not args.url and not args.api:
+        raise Exception('api-key is required')
     if len(paths) == 0:
         print('File {} does not exist.'.format(args.FILE))
         return
     for path in paths:
         with open(path, 'rb') as fp:
             if args.url:
-                response = requests.post(args.url + '/alpr', files=dict(upload=fp), data=dict(regions=region))
+                response = requests.post(args.url + '/alpr', files=dict(upload=fp), data=dict(regions=regions))
             else:    
                 response = requests.post(
                     'https://api.platerecognizer.com/v1/plate-reader/',
                     files=dict(upload=fp),
-                    data=dict(regions=region),
+                    data=dict(regions=regions),
                     headers={'Authorization': 'Token ' + args.api})
             result.append(response.json(object_pairs_hook=OrderedDict))
             time.sleep(1)
