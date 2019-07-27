115367fe7055f5c65f76d8f7abfcb32baec69c6b
Muhia John NJoroge <clbnjoroge@gmail.com>
2019-07-27 11:29:52 +0300

    remove docker/sdk_integration.py and update plate_recognition.py to handle its functionality

--- plate_recognition.py
+++ plate_recognition.py
@@ -18,6 +18,8 @@ def parse_arguments():
         'For example: python plate_recognition.py --api MY_API_KEY "/path/to/vehicle-*.jpg"'
     )
     parser.add_argument('--api', help='Your API key.', required=True)
+    parser.add_argument('--region', help='Match the license plate pattern fo specific region', required=False)
+    parser.add_argument('--url', default='http://localhost:8080', help="Url to self hosted sdk", required=False)
     parser.add_argument('files', nargs='+', help='Path to vehicle image or pattern.')
     return parser.parse_args()
 
@@ -26,17 +28,22 @@ def main():
     args = parse_arguments()
     result = []
     paths = args.files
+    region = args.region
     if len(paths) == 0:
         print('File {} does not exist.'.format(args.FILE))
         return
     for path in paths:
         with open(path, 'rb') as fp:
-            response = requests.post(
-                'https://api.platerecognizer.com/v1/plate-reader/',
-                files=dict(upload=fp),
-                headers={'Authorization': 'Token ' + args.api})
+            if args.url:
+                response = requests.post(args.url + '/alpr', files=dict(upload=fp), data=dict(regions=region))
+            else:    
+                response = requests.post(
+                    'https://api.platerecognizer.com/v1/plate-reader/',
+                    files=dict(upload=fp),
+                    data=dict(regions=region),
+                    headers={'Authorization': 'Token ' + args.api})
             result.append(response.json(object_pairs_hook=OrderedDict))
-        time.sleep(1)
+            time.sleep(1)
     print(json.dumps(result, indent=2))
 
 
