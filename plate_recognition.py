5caaba41aca4d92b9bbf0f396fd53731d94ecb91
Marc Belmont <marcbelmont@users.noreply.github.com>
2019-12-06 09:57:51 +0100

    Add a delay based on response status code

--- plate_recognition.py
+++ plate_recognition.py
@@ -5,7 +5,6 @@ import argparse
 import json
 import time
 from collections import OrderedDict
-from glob import glob
 
 import requests
 
@@ -17,9 +16,16 @@ def parse_arguments():
         epilog=
         'For example: python plate_recognition.py --api-key MY_API_KEY "/path/to/vehicle-*.jpg"'
     )
-    parser.add_argument('--api', '--api-key', help='Your API key.', required=False)
-    parser.add_argument('--regions', help='Match the license plate pattern fo specific region', required=False, action="append")
-    parser.add_argument('--url','--sdk-url', help="Url to self hosted sdk  For example, http://localhost:8080", required=False)
+    parser.add_argument('--api-key', help='Your API key.', required=False)
+    parser.add_argument(
+        '--regions',
+        help='Match the license plate pattern fo specific region',
+        required=False,
+        action="append")
+    parser.add_argument(
+        '--sdk-url',
+        help="Url to self hosted sdk  For example, http://localhost:8080",
+        required=False)
     parser.add_argument('files', nargs='+', help='Path to vehicle images')
     return parser.parse_args()
 
@@ -30,23 +36,29 @@ def main():
     paths = args.files
     regions = args.regions
 
-    if not args.url and not args.api:
+    if not args.sdk_url and not args.api_key:
         raise Exception('api-key is required')
     if len(paths) == 0:
         print('File {} does not exist.'.format(args.FILE))
         return
     for path in paths:
         with open(path, 'rb') as fp:
-            if args.url:
-                response = requests.post(args.url + '/alpr', files=dict(upload=fp), data=dict(regions=regions))
-            else:    
-                response = requests.post(
-                    'https://api.platerecognizer.com/v1/plate-reader/',
-                    files=dict(upload=fp),
-                    data=dict(regions=regions),
-                    headers={'Authorization': 'Token ' + args.api})
+            if args.sdk_url:
+                response = requests.post(args.sdk_url + '/alpr',
+                                         files=dict(upload=fp),
+                                         data=dict(regions=regions))
+            else:
+                for _ in range(3):
+                    response = requests.post(
+                        'https://api.platerecognizer.com/v1/plate-reader/',
+                        files=dict(upload=fp),
+                        data=dict(regions=regions),
+                        headers={'Authorization': 'Token ' + args.api_key})
+                    if response.status_code == 429:  # Max calls per second reached
+                        time.sleep(1)
+                    else:
+                        break
             result.append(response.json(object_pairs_hook=OrderedDict))
-            time.sleep(1)
     print(json.dumps(result, indent=2))
 
 
