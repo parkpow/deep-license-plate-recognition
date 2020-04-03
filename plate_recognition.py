ca41593d0dd024276f4b309eab9b5afc81a96ca0
Marc Belmont <marcbelmont@users.noreply.github.com>
2020-04-03 18:04:06 +0200

    Add option to save output as CSV

--- plate_recognition.py
+++ plate_recognition.py
@@ -2,6 +2,8 @@
 from __future__ import absolute_import, division, print_function
 
 import argparse
+import collections
+import csv
 import json
 import math
 import time
@@ -20,16 +22,26 @@ def parse_arguments(args_hook=lambda _: _):
         'To send images to our cloud service: '
         'python plate_recognition.py --api-key MY_API_KEY /path/to/vehicle-*.jpg\n',
         formatter_class=argparse.RawTextHelpFormatter)
-    parser.add_argument('--api-key', help='Your API key.', required=False)
+    parser.add_argument('-a', '--api-key', help='Your API key.', required=False)
     parser.add_argument(
+        '-r',
         '--regions',
         help='Match the license plate pattern fo specific region',
         required=False,
         action="append")
     parser.add_argument(
+        '-s',
         '--sdk-url',
         help="Url to self hosted sdk  For example, http://localhost:8080",
         required=False)
+    parser.add_argument('--camera-id',
+                        help="Name of the source camera.",
+                        required=False)
+
+    parser.add_argument(
+        '--mmc',
+        action='store_true',
+        help='Predict vehicle make and model (SDK only). It has to be enabled.')
     parser.add_argument('files', nargs='+', help='Path to vehicle images')
     args_hook(parser)
     args = parser.parse_args()
@@ -38,8 +50,21 @@ def parse_arguments(args_hook=lambda _: _):
     return args
 
 
-def recognition_api(fp, regions=[], api_key=None, sdk_url=None, config={}):
+def recognition_api(fp,
+                    regions=[],
+                    api_key=None,
+                    sdk_url=None,
+                    config={},
+                    camera_id=None,
+                    timestamp=None,
+                    mmc=None):
     data = dict(regions=regions, config=json.dumps(config))
+    if camera_id:
+        data['camera_id'] = camera_id
+    if mmc:
+        data['mmc'] = mmc
+    if timestamp:
+        data['timestamp'] = timestamp
     if sdk_url:
         response = requests.post(sdk_url + '/alpr',
                                  files=dict(upload=fp),
@@ -93,8 +118,63 @@ def draw_bb(im, data):
     return im
 
 
-def blurring_args(parser):
-    parser.epilog += 'To blur images: python plate_recognition.py  --sdk-url http://localhost:8080 --blur-amount 4 --blur-plates /path/to/vehicle-*.jpg'
+def flatten_dict(d, parent_key='', sep='_'):
+    items = []
+    for k, v in d.items():
+        new_key = parent_key + sep + k if parent_key else k
+        if isinstance(v, collections.MutableMapping):
+            items.extend(flatten_dict(v, new_key, sep=sep).items())
+        else:
+            if isinstance(v, list):
+                items.append((new_key, json.dumps(v)))
+            else:
+                items.append((new_key, v))
+    return dict(items)
+
+
+def flatten(result):
+    plates = result['results']
+    del result['results']
+    del result['usage']
+    if not plates:
+        return result
+    for plate in plates:
+        data = result.copy()
+        data.update(flatten_dict(plate))
+    return data
+
+
+def save_results(results, args):
+    path = Path(args.output_file)
+    if not path.parent.exists():
+        print('%s does not exist' % path)
+        return
+    if not results:
+        return
+    if args.format == 'json':
+        with open(path, 'w') as fp:
+            json.dump(results, fp)
+    elif args.format == 'csv':
+        fieldnames = []
+        for result in results[:10]:
+            candidate = flatten(result.copy()).keys()
+            if len(fieldnames) < len(candidate):
+                fieldnames = candidate
+        with open(path, 'w') as fp:
+            writer = csv.DictWriter(fp, fieldnames=fieldnames)
+            writer.writeheader()
+            for result in results:
+                writer.writerow(flatten(result))
+
+
+def custom_args(parser):
+    parser.epilog += 'To blur images: python plate_recognition.py --sdk-url http://localhost:8080 --blur-amount 4 --blur-plates /path/to/vehicle-*.jpg\n'
+    parser.epilog += 'To save results: python plate_recognition.py --sdk-url http://localhost:8080 -o data.csv --format csv /path/to/vehicle-*.jpg\n'
+    parser.add_argument('-o', '--output-file', help='Save result to file.')
+    parser.add_argument('--format',
+                        help='Format of the result.',
+                        default='json',
+                        choices='json csv'.split())
     parser.add_argument(
         '--blur-amount',
         help=
@@ -110,22 +190,29 @@ def blurring_args(parser):
 
 
 def main():
-    args = parse_arguments(blurring_args)
+    args = parse_arguments(custom_args)
     paths = args.files
 
-    result = []
+    results = []
     for path in paths:
         with open(path, 'rb') as fp:
-            api_res = recognition_api(fp, args.regions, args.api_key,
-                                      args.sdk_url)
+            api_res = recognition_api(fp,
+                                      args.regions,
+                                      args.api_key,
+                                      args.sdk_url,
+                                      camera_id=args.camera_id,
+                                      mmc=args.mmc)
         if args.blur_plates:
             im = blur(Image.open(path), args.blur_amount, api_res)
             filename = Path(path)
             im.save(filename.parent / ('%s_blurred%s' %
                                        (filename.stem, filename.suffix)))
 
-        result.append(api_res)
-    print(json.dumps(result, indent=2))
+        results.append(api_res)
+    if args.output_file:
+        save_results(results, args)
+    else:
+        print(json.dumps(results, indent=2))
 
 
 if __name__ == '__main__':
