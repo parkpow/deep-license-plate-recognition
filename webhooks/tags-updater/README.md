2eb25957393642a0017e96c50251c8357ad5fbc6
nyaundi brian <danleyb2@gmail.com>
2022-01-04 11:14:04 +0300

    Review Fixes

--- webhooks/tags-updater/README.md
+++ webhooks/tags-updater/README.md
@@ -1,6 +1,10 @@
 
 1. Create Config
  ```ini
+[settings]
+parkpow_base_url = https://app.parkpow.com
+parkpow_api_token = 8003dfa697f08f07d108c10de6f7cf17d86707a5
+
 [tag-update]
 OLD = NEW
 SILVER = SILVERPROC
@@ -11,50 +15,37 @@ NO_TAGS = NEWCLIENT
 
 2. Build the image
 ```bash
-docker build --tag platerecognizer/tags-updater .
+docker build --tag platerecognizer/parkpow-tag-updater .
 ```
 
 3. Run Image
-> Configure ParkPow API TOKEN and BASE_URL
+Development
 ```bash
 
-docker run --rm -i -v ${PWD}:/code -p 3000:8001 -e TOKEN=8003########################## -e BASE_URL=https://app.parkpow.com platerecognizer/tags-updater
+docker run --rm -i -v ${PWD}:/app -p 3000:8001 platerecognizer/parkpow-tag-updater
 
-# Verbose logging
-docker run --rm -i -v ${PWD}:/code -p 3000:8001 -e TOKEN=8003########################## -e BASE_URL=https://app.parkpow.com -e LOGGING=DEBUG  platerecognizer/tags-updater
+# Verbose logging add -e LOGGING=DEBUG
+docker run --rm -i -v ${PWD}:/app -p 3000:8001 -e LOGGING=DEBUG  platerecognizer/parkpow-tag-updater
 
-# Custom Config
-docker run --rm -i -v /tmp/config.ini:/code/config.ini -v ${PWD}:/code -p 3000:8001 -e TOKEN=8003########################## -e BASE_URL=https://app.parkpow.com -e LOGGING=DEBUG  platerecognizer/tags-updater
+# Custom Config add -v /tmp/config.ini:/app/config.ini
+docker run --rm -i  -v ${PWD}:/app -p 3000:8001 -v /tmp/config.ini:/app/config.ini  platerecognizer/parkpow-tag-updater
 
 ```
 
+Production
+```bash
+
+docker run --rm -i -p 3000:8001 platerecognizer/parkpow-tag-updater
+
+# Verbose logging add -e LOGGING=DEBUG
+docker run --rm -i -p 3000:8001 -e LOGGING=DEBUG  platerecognizer/parkpow-tag-updater
+
+# Custom Config add -v /tmp/config.ini:/app/config.ini
+docker run --rm -i  -p 3000:8001 -v /tmp/config.ini:/app/config.ini  platerecognizer/parkpow-tag-updater
+
+```
 
 5. Send a MANUAL Alert
-
-```json
-
-{
-"alert_name": "Test",
-"time": "3 January 2022 at 10:28",
-"timezone": "UTC",
-"site": "Default Site",
-"camera": "4958",
-"license_plate": "nhk552",
-"visits":69,
-"confidence_level": 0.84,
-"vehicle_id": 1204911,
-"vehicle_type": "Sedan",
-"vehicle_tag": "(empty)",
-"vehicle_make": "Riley",
-"vehicle_model": "RMF",
-"vehicle_url":"http://app.parkpow.com/vehicle/1204911",
-"vehicle_color": "Black",
-"message": "Test",
-"photo": "https://us-east-1.linodeobjects.com/parkpow-web/4958/2022-01-03/1028_XSQ82_1028_oHqIZ_car.jpg"
-}
-
-```
-
 Empty Tags
 ```bash
 curl -X 'POST' 'http://localhost:3000'\
@@ -76,12 +67,12 @@ Single Tag SILVER
 curl -X 'POST' 'http://localhost:3000'\
     -H 'connection: close' \
     -H 'content-type: application/x-www-form-urlencoded' \
-    -H 'content-length: 438' \
+    -H 'content-length: 456' \
     -H 'accept: */*' \
     -H 'accept-encoding: gzip, deflate' \
     -H 'user-agent: python-requests/2.26.0' \
     -H 'host: webhook.site' \
-    -d 'alert_name=Test&time=3+January+2022+at+13%3A24&timezone=UTC&site=Default+Site&camera=4958&license_plate=nhk552&visits=70&confidence_level=0.84&vehicle_id=1204911&vehicle_type=Sedan&vehicle_tag=&vehicle_make=Riley&vehicle_model=RMF&vehicle_url=http%3A%2F%2Fapp.parkpow.com%2Fvehicle%2F1204911&vehicle_color=Black&message=Test&photo=https%3A%2F%2Fus-east-1.linodeobjects.com%2Fparkpow-web%2F4958%2F2022-01-03%2F1324_XL5Py_1324_nLq7Q_car.jpg'
+    -d 'alert_name=Test&alert_tags=&time=4+January+2022+at+07%3A10&timezone=UTC&site=Default+Site&camera=4958&license_plate=nhk552&visits=72&confidence_level=0.84&vehicle_id=1204911&vehicle_type=Sedan&vehicle_tag=SILVER&vehicle_make=Riley&vehicle_model=RMF&vehicle_url=http%3A%2F%2Fapp.parkpow.com%2Fvehicle%2F1204911&vehicle_color=Black&message=Test&photo=https%3A%2F%2Fus-east-1.linodeobjects.com%2Fparkpow-web%2F4958%2F2022-01-04%2F0710_rrhg4_0710_gjBAP_car.jpg'
 
 
 ```
--- webhooks/tags-updater/main.py
+++ webhooks/tags-updater/main.py
@@ -13,6 +13,7 @@ LOG_LEVEL = os.environ.get('LOGGING', 'INFO').upper()
 logging.basicConfig(
     stream=sys.stdout,
     level=LOG_LEVEL,
+    style='{',
     format="{asctime} {levelname} {name} {threadName} : {message}")
 
 lgr = logging.getLogger(__name__)
@@ -25,24 +26,27 @@ def read_config():
     """
     parser = configparser.ConfigParser()
     lgr.debug('Reading Configuration File')
-    parser.read('config.ini')
     try:
-        return parser['tag-update']
+        parser.read('config.ini')
+        settings = parser['settings']
+        tag_update = parser['tag-update']
+        return settings, tag_update
     except Exception:
         lgr.error('Invalid Configuration File')
 
 
-def update_vehicle_tag(vehicle_id, tag, check, api_token):
+def update_vehicle_tag(vehicle_id, tag, check, api_token, base_url):
     """
     Update ParkPow Vehicle TAGS
 
     :param vehicle_id:
     :param tag:
     :param check:
+    :param api_token:
+    :param base_url:
     :return:
     """
     lgr.debug(f'update_vehicle_tag({vehicle_id},{tag},{check})')
-    base_url = os.getenv('BASE_URL', "https://app.parkpow.com")
 
     res = requests.post(f'{base_url}/api/v1/edit-tags/',
                         params={
@@ -58,35 +62,37 @@ def update_vehicle_tag(vehicle_id, tag, check, api_token):
     return res
 
 
-def process_alert(config, raw_data, parkpow_api_token):
+def process_alert(config, raw_data, parkpow_api_token, parkpow_base_url):
     """
     Select vehicle_tag from Alert, Check Config and Make Updates
 
-    :param raw_data:
     :param config:
+    :param raw_data:
+    :param parkpow_api_token:
+    :param parkpow_base_url:
     :return:
     """
     data = parse_qs(raw_data)
     lgr.debug(json.dumps(data, indent=2))
 
     if 'vehicle_tag' in data:
-        vehicle_tag = data['vehicle_tag']
+        vehicle_tag = data['vehicle_tag'][0]
         lgr.debug(f'vehicle_tag: {vehicle_tag}')
 
         if vehicle_tag in config:
             new_vehicle_tag = config[vehicle_tag]
             lgr.debug(f'new_vehicle_tag: {new_vehicle_tag}')
 
-            vehicle_id = data['vehicle_id']
+            vehicle_id = data['vehicle_id'][0]
             lgr.debug(f'vehicle_id: {vehicle_id}')
 
             # Removing old Tag
             update_vehicle_tag(vehicle_id, vehicle_tag, False,
-                               parkpow_api_token)
+                               parkpow_api_token, parkpow_base_url)
 
             # Adding new Tag
             update_vehicle_tag(vehicle_id, new_vehicle_tag, True,
-                               parkpow_api_token)
+                               parkpow_api_token, parkpow_base_url)
 
         else:
             lgr.debug('Skipped missing vehicle tag')
@@ -119,7 +125,8 @@ class AlertRequestHandler(BaseHTTPRequestHandler):
             lgr.debug(f'raw_data: {raw_data}')
 
             process_alert(self.server.config, raw_data,
-                          self.server.parkpow_api_token)
+                          self.server.parkpow_api_token,
+                          self.server.parkpow_base_url)
 
         else:
             lgr.error('Invalid content type')
@@ -132,21 +139,22 @@ class TagsUpdaterServer(HTTPServer):
     def __init__(self,
                  server_address,
                  config,
-                 parkpow_api_token,
+                 settings,
                  handler_class=AlertRequestHandler):
         super().__init__(server_address, handler_class)
         self.config = config
-        self.parkpow_api_token = parkpow_api_token
+        self.parkpow_api_token = settings['parkpow_api_token']
+        self.parkpow_base_url = settings.get('parkpow_base_url',
+                                             'https://app.parkpow.com')
 
 
 if __name__ == '__main__':
-    config = read_config()
-    parkpow_api_token = os.getenv('TOKEN', None)
-    if parkpow_api_token is None:
-        lgr.error('Missing required env variable TOKEN')
+    settings, config = read_config()
+    if not settings['parkpow_api_token']:
+        lgr.error('Missing required parkpow_api_token')
         exit(1)
 
-    server = TagsUpdaterServer(('', 8001), config, parkpow_api_token,
+    server = TagsUpdaterServer(('', 8001), config, settings,
                                AlertRequestHandler)
     lgr.info('Starting server, use <Ctrl-C> to stop')
     server.serve_forever()
