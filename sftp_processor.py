fdaecd709f91a66f706af5443d4de3eff7a3f621
nyaundi brian <danleyb2@gmail.com>
2022-08-10 02:26:47 +0300

    Add interval
    Signed-off-by: nyaundi brian <danleyb2@gmail.com>

--- sftp_processor.py
+++ sftp_processor.py
@@ -8,6 +8,7 @@ from plate_recognition import recognition_api
 import os
 import sys
 from stat import S_ISDIR, S_ISREG
+import time
 
 LOG_LEVEL = os.environ.get('LOGGING', 'INFO').upper()
 
@@ -20,9 +21,11 @@ logging.basicConfig(
 
 lgr = logging.getLogger(__name__)
 
+# Keep track of processed file names
+processed = []
+
 
 def get_connection(hostname, username, port, password=None, pkey_path=None):
-
     ssh = paramiko.SSHClient()
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
@@ -36,34 +39,59 @@ def get_connection(hostname, username, port, password=None, pkey_path=None):
     return sftp
 
 
+def track_processed(args):
+    """
+    Track processed is an interval specified
+
+    :param args:
+    :return:
+    """
+    return args.interval and args.interval > 0
+
+
 def main(args):
     """
 
     :param args: args from argparse
     :return:
     """
-    sftp = get_connection(args.host, args.user, args.port, password=args.password, pkey_path=args.pkey)
-    root = Path(args.folder)
+    sftp = None
+    try:
+        sftp = get_connection(args.host, args.user, args.port, password=args.password, pkey_path=args.pkey)
+        root = Path(args.folder)
 
-    for entry in sftp.listdir_attr(str(root)):
-        remote_path = root / entry.filename
-        mode = entry.st_mode
-        if S_ISDIR(mode):
-            # Skip Dir
-            pass
-        elif S_ISREG(mode):
-            with tempfile.NamedTemporaryFile(suffix='_' + entry.filename, mode='rb+') as image:
-                sftp.getfo(str(remote_path), image)
+        for entry in sftp.listdir_attr(str(root)):
+            if track_processed(args) and entry.filename in processed:
+                lgr.debug(f'skip processed file: {entry.filename}')
+                continue
 
-                api_res = recognition_api(image,
-                                          args.regions,
-                                          args.api_token,
-                                          args.sdk_url,
-                                          camera_id=args.camera_id,
-                                          mmc=args.mmc,
-                                          exit_on_error=False)
+            remote_path = root / entry.filename
+            mode = entry.st_mode
+            if S_ISDIR(mode):
+                # Skip Dir
+                pass
+            elif S_ISREG(mode):
+                with tempfile.NamedTemporaryFile(suffix='_' + entry.filename, mode='rb+') as image:
+                    sftp.getfo(str(remote_path), image)
 
-                lgr.info(api_res)
+                    api_res = recognition_api(image,
+                                              args.regions,
+                                              args.api_token,
+                                              args.sdk_url,
+                                              camera_id=args.camera_id,
+                                              mmc=args.mmc,
+                                              exit_on_error=False)
+
+                    lgr.info(api_res)
+
+                if track_processed(args):
+                    processed.append(entry.filename)
+
+    except Exception as e:
+        lgr.error(e)
+    finally:
+        if sftp:
+            sftp.close()
 
 
 if __name__ == '__main__':
@@ -78,7 +106,9 @@ if __name__ == '__main__':
                      "Private Key login, Process images in /tmp/images: " \
                      "./sftp_processor.py -U usr1  --pkey '/home/danleyb2/.ssh/id_rsa' -H 192.168.100.21 -f /tmp/images -a 4805bee#########\n" \
                      "Password login, Process images in /tmp/images using Snapshot SDK: " \
-                     "./sftp_processor.py -U usr1  -P pass -H 192.168.100.21 -f /tmp/images -s http://localhost:8080"
+                     "./sftp_processor.py -U usr1  -P pass -H 192.168.100.21 -f /tmp/images -s http://localhost:8080\n" \
+                     "Process images in /tmp/images Periodically every 5 seconds: " \
+                     "./sftp_processor.py -U usr1  -P pass -H 192.168.100.21 -f /tmp/images -a 4805bee######### -i 5"
 
     parser.add_argument('-a', '--api-token', help='Cloud API Token.', required=False)
     parser.add_argument(
@@ -110,9 +140,20 @@ if __name__ == '__main__':
         '--mmc',
         action='store_true',
         help='Predict vehicle make and model (SDK only). It has to be enabled.')
+
+    parser.add_argument(
+        '-i',
+        '--interval',
+        type=int,
+        help='Periodically fetch new images from the server every interval seconds.')
     cli_args = parser.parse_args()
 
     if not cli_args.sdk_url and not cli_args.api_token:
         raise Exception('api-key is required')
 
-    main(cli_args)
+    if cli_args.interval and cli_args.interval > 0:
+        while True:
+            main(cli_args)
+            time.sleep(cli_args.interval)
+    else:
+        main(cli_args)
