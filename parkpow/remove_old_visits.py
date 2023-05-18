f41571349e22448b4df43cb4094cfd2d40fd3671
adolfo <adolfoarmas.90@gmail.com>
2023-05-18 19:52:02 -0300

    async remove old visits script

--- parkpow/remove_old_visits.py
+++ parkpow/remove_old_visits.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python
+import asyncio
+import http.client
 import argparse
 import requests
 import datetime
+import json
 from urllib.parse import urlparse, parse_qs
-
 
 def get_headers(authentication_token):
     """headers building to include in requests functions
@@ -16,21 +18,84 @@ def get_headers(authentication_token):
     """
     return {"Authorization": f"Token {authentication_token}"}
 
+async def api_fetch(url, headers=None, params=None):
+    """Asynchronous API fetch function
 
-def list_visits(api_key, max_age, hostname, page=None):
-    """fetchs the visits from the past <max_age> days.
+    Args:
+        url (str): hostname + API path
+        headers (dict, optional): Required headers as Authorization. Defaults to None.
+        params (dict, optional): Request to set url parameters. Defaults to None.
+
+    Returns:
+        dict: response <HTTPResponse class>, connection <HTTPSConnection or HTTPConnection class>
+    """
+    parsed_url = urlparse(url)
+
+    if parsed_url.scheme == "http":
+        connection = http.client.HTTPConnection(parsed_url.hostname)    
+    elif parsed_url.scheme == "https":
+        connection = http.client.HTTPSConnection(parsed_url.hostname)
+    else:
+        print("URL is not using HTTP or HTTPS")
+    
+    if params:
+        api_path = f"{parsed_url.path}"
+        query_string = '&'.join([f"{key}={value}" for key, value in params.items()])
+        url_with_params = f"{api_path}?{query_string}"
+        
+    try: 
+        connection.request("GET", url_with_params, headers=headers)
+        response = await asyncio.get_event_loop().run_in_executor(None, connection.getresponse)
+        return {'response':response, 'connection':connection}
+    
+    except Exception as e:
+        print(f"Error: {str(e)}")
+        return None
+
+async def api_post(url, data=None, headers=None):
+    """Asynchronous API post function
+       
+    Args:
+        url (str): hostname + API path
+        data (dict, optional): payload to send.
+        headers (dict, optional): Required headers as Authorization. Defaults to None.
+
+    Returns:
+        dict: response <HTTPResponse class>, connection <HTTPSConnection or HTTPConnection class>
+    """
+    parsed_url = urlparse(url)
+
+    if parsed_url.scheme == "http":
+        connection = http.client.HTTPConnection(parsed_url.hostname)
+    elif parsed_url.scheme == "https":
+        connection = http.client.HTTPSConnection(parsed_url.hostname)
+    else:
+        print("URL is not using HTTP or HTTPS")
+
+    api_path = parsed_url.path
+    
+    try:
+        connection.request("POST", api_path, json.dumps(data), headers)
+        response = await asyncio.get_event_loop().run_in_executor(None, connection.getresponse)
+        return {'response':response, 'connection':connection}
+    
+    except Exception as e:
+        print("Error:", e)
+
+async def list_visits(api_key, max_age, hostname, page=None):
+    """lists the visits from the past <max_age> days.
 
     Args:
         api_key (str): Park-Pow API key
-        max_age (str): Quantity of days back from today to be deleted
+        max_age (str): Quantity of days back from today to be removed
         hostname (str): https://<host_name>/api/v1/, For example:https://app.parkpow.com/api/v1/ or http://local-or-public-IP:8000/api/v1/
         page (str, optional): API pagination number comming in data['next']. Defaults to None in first request.
 
     Returns:
-        JSON: response from API
+        dict: response <HTTPResponse class>, connection <HTTPSConnection or HTTPConnection class>
     """
-    hostname = hostname
-    path = "visit-list"
+    api_url = hostname
+    visit_list_path = "visit-list/"
     api_key = api_key
     max_age = max_age
     end_period = datetime.date.today()
@@ -46,58 +111,91 @@ def list_visits(api_key, max_age, hostname, page=None):
 
     headers = get_headers(api_key)
 
-    try:
-        response = requests.get(
-            hostname + path, params=params, headers=headers)
-        print(f"Fetching visit, page {'1' if not page else page}:")
-        response.raise_for_status()
-        data = response.json()
+    print(f"Fetching visit, page {'1' if not page else page}:")
+    response_and_connection = await api_fetch(api_url + visit_list_path, headers, params)
+    
+    expected_status_code = 200
+    if response_and_connection['response'].status == expected_status_code:
+        response_data = await asyncio.get_event_loop().run_in_executor(None, response_and_connection['response'].read)
+        data = json.loads(response_data.decode())
+        response_and_connection['connection'].close()
         return data
+    else:
+        raise Exception(f"Request failed. Expected status code: {expected_status_code}. Actual status code: {response_and_connection['response'].status}.\x0aCheck parameters and try again...")
 
-    except requests.exceptions.RequestException as e:
-        print("Error occurred while retrieving visit list:", str(e))
-        return None
-
-
-def delete_visit(hostname, api_key, visit_id):
-    """delete individual visits by <visit_id>
+async def remove_visit(hostname, api_key, visit_id):
+    """remove individual visits by <visit_id>
 
     Args:
         hostname (str): https://<host_name>/api/v1/, For example:https://app.parkpow.com/api/v1/ or http://local-or-public-IP:8000/api/v1/
         api_key (str): Park-Pow API key
-        visit_id (str): visit identification
+        visit_id (str): visit identification str(<int>)
 
     Returns:
         JSON: response from API
     """
+    api_url = hostname
     headers = get_headers(api_key)
-    path = "delete-visit/"
+    headers['Content-Type'] = "application/json"
+    
+    remove_visit_path = "delete-visit/"
     payload = {
         "id": visit_id,
     }
-    try:
-        response = requests.post(
-            hostname + path, json=payload, headers=headers)
-        response.raise_for_status()
+    response_and_connection = await api_post(api_url + remove_visit_path, payload, headers)
 
-        expected_status_code = 200
-        if response.status_code == expected_status_code:
-            print(f"Visit {visit_id} successfully deleted!")
-        else:
-            print(
-                f"Request failed. Expected status code: {expected_status_code}. Actual status code: {response.status_code}"
+    expected_status_code = 200
+    if response_and_connection['response'].status == expected_status_code:
+        print(f"Visit {visit_id} successfully removed!")
+    else:
+        raise Exception(f"Request failed. Expected status code: {expected_status_code}. Actual status code: {response_and_connection['response'].status}.\x0aReason: {response_and_connection['response'].reason}")
+    
+    response_data = await asyncio.get_event_loop().run_in_executor(None, response_and_connection['response'].read)
+    data = json.loads(response_data.decode())
+    response_and_connection['connection'].close()
+    return data
+
+async def main():
+    
+    visits_list = []
+    removed_count = 0
+    visits = await list_visits(cli_args.token, cli_args.max_age, cli_args.api_url)
+    if any(visits["results"]):
+        while True:
+            visits_list.extend(
+                [
+                    {"id": object["id"],
+                        "start_date": object["start_date"]}
+                    for object in visits["results"]
+                ]
             )
+            if visits["next"]:
+                parsed_url = urlparse(visits["next"])
+                query_params = parse_qs(parsed_url.query)
+                if "page" in query_params:
+                    page_value = query_params["page"][0]
+                visits = await list_visits(
+                    cli_args.token, cli_args.max_age, cli_args.api_url, page_value
+                )
+            else:
+                break
 
-        data = response.json()
-        return data
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error occurred while deleting visit {visit_id} :", str(e))
-        return None
-
-
+        estimated_count = visits["estimated_count"]
+        print(f"{estimated_count} visits to be removed...")
+        #reverse=False, oldest first | reverse=True newest first
+        sorted_visit_list = sorted(visits_list, key=lambda x: x['start_date'], reverse=False)
+        for visit in sorted_visit_list:
+            id = visit["id"]
+            start_date = visit["start_date"]
+            print(f"Deleting visit {id}, date-time: {start_date}")
+            await remove_visit(cli_args.api_url, cli_args.token, visit["id"])
+            removed_count += 1
+        print(f'Completed, {str(removed_count)} of {estimated_count} visits removed')
+    else:
+        print("Empty list: No visits to remove")
+        
 if __name__ == "__main__":
-    # python remove_old_visits.py --token KEY --max-age 30 --api-url https://app.parkpow.com/api/v1/
+    
     parser = argparse.ArgumentParser(
         description="Remove old visits up to 30 days back in Park Pow historical register",
         epilog="",
@@ -121,7 +219,7 @@ if __name__ == "__main__":
     parser.add_argument(
         "-m",
         "--max-age",
-        help="Number of previous days you want to delete.",
+        help="Number of previous days you want to remove.",
         required=False,
     )
 
@@ -142,38 +240,6 @@ if __name__ == "__main__":
         raise Exception("max-age is a 1 to 30 required integer")
     if not cli_args.api_url:
         raise Exception("api-url is required")
-    else:
-        visits_list = []
-        visits = list_visits(
-            cli_args.token, cli_args.max_age, cli_args.api_url)
-        if any(visits["results"]):
-            while True:
-                visits_list.extend(
-                    [
-                        {"id": object["id"],
-                            "start_date": object["start_date"]}
-                        for object in visits["results"]
-                    ]
-                )
-                if visits["next"]:
-                    parsed_url = urlparse(visits["next"])
-                    query_params = parse_qs(parsed_url.query)
-                    if "page" in query_params:
-                        page_value = query_params["page"][0]
-                    visits = list_visits(
-                        cli_args.token, cli_args.max_age, cli_args.api_url, page_value
-                    )
-                else:
-                    break
 
-            estimated_count = visits["estimated_count"]
-            print(f"{estimated_count} visits to be deleted...")
-            visits_list.reverse() #oldest
-            for visit in visits_list:
-                id = visit["id"]
-                start_date = visit["start_date"]
-                print(f"Deleting visit {id}, date-time: {start_date}")
-                delete_visit(cli_args.api_url, cli_args.token, visit["id"])
-            print('Completed!')
-        else:
-            print("Empty list: No visits to delete")
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
