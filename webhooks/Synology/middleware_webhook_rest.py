e2d7629d11e72c743316681bd8abfcad0a8fc286
kevinchan <kevinchan@synology.inc>
2024-03-20 14:00:38 +0800

    fix: (synology) correct the timestamp parsing error

--- webhooks/Synology/middleware_webhook_rest.py
+++ webhooks/Synology/middleware_webhook_rest.py
@@ -5,32 +5,24 @@ import base64
 from urllib.parse import parse_qs
 from urllib import parse
 from http.server import BaseHTTPRequestHandler, HTTPServer
-from datetime import datetime
 import requests
 import os
+import dateutil.parser as dp
 
 class GetHandler(BaseHTTPRequestHandler):
-    def is_timestamp(self, timestamp):
-	try:
-		format_string = '%Y-%m-%d %H:%M:%S.%f%z'
-		dt_object = dt_object = datetime.strptime(timestamp, format_string)
-		return dt_object
-	except ValueError:
-		return None
-                
-    def forward_to_rest_service(self, json_data):
+    def forward_to_rest_service(self, json_data, image_base64):
         url = os.getenv('REST_SERVICE_URL')
 
         # The data from the plate regognizer
         timestamp = json_data['data']['timestamp_local']
         plate = json_data['data']['results'][0]['plate']
 
-	dt_object = self.is_timestamp(timestamp)
-        if dt_object:
-		timestamp = str(dt_object.timestamp())
-	else:
-		print("\033[31mERROR:	Invalid timestamp format.%s\033[0m", timestamp)
-		return
+        try:
+            parsed_date_time = dp.parse(timestamp)
+            timestamp = str(int(parsed_date_time.timestamp()))
+        except ValueError:
+            print("\033[31mERROR:    Invalid timestamp format.%s\033[0m", timestamp)
+            return
 
         # SVS support time, text1, text2, text3 to be the keys
         request_data = {
