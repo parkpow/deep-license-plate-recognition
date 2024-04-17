9539b6a6ec42c026fc5e857402d6d061ab8874a0
nyaundi brian <danleyb2@gmail.com>
2024-04-17 19:26:06 +0300

    feat: (verkada-parkpow-visits) host service on cloudflare workers

---
+++ parkpow/verkada-lpr-webhooks/README.md
@@ -0,0 +1,42 @@
+# verkada-lpr-webhooks
+
+## Deployment steps
+> The cloudflare account needs to have a paid plan for workers to be able to create queues
+
+1. Create a [Cloudflare queue](https://developers.cloudflare.com/queues/get-started/#3-create-a-queue) that will hold webhook messages.
+Can be done by using below command or manually in the dashboard
+```shell
+npx wrangler queues create <MY_FIRST_QUEUE>
+```
+
+2. Deploy the worker by running below command
+> this will prompt you to login the first time
+
+A url will be generated to be used as the webhook target on Verkada API settings page
+```shell
+npm run deploy
+```
+
+3. Update the application env variables with the following values
+> This will redeploy the worker and persist any future deployments
+> For added security, Click on the Encrypt button on each variable
+
+```shell
+# Verkada API Key
+VERKADA_API_KEY=
+# ParkPow Token
+PARKPOW_TOKEN=
+# ParkPow Server URL
+PARKPOW_URL=
+
+```
+4. Run below command to get realtime logs
+```shell
+npm run logs
+```
+
+## Local development
+- Create a `.dev.vars` with the env variables required
+- Run `npm run dev` in your terminal to start a development server
+- Open a browser tab at http://localhost:8787/ to see your worker in action
+
---
+++ parkpow/verkada-lpr-webhooks/src/index.js
@@ -0,0 +1,187 @@
+class ParkPowApi {
+  constructor(token, sdkUrl = null) {
+    if (token === null) {
+      throw new Error("ParkPow TOKEN is required if using Cloud API");
+    } else {
+      this.token = token;
+    }
+    if (sdkUrl) {
+      this.apiBase = sdkUrl + "/api/v1/";
+    } else {
+      this.apiBase = "https://app.parkpow.com/api/v1/";
+    }
+    console.debug("Api Base: " + this.apiBase);
+  }
+
+  async logVehicle(
+    encodedImage,
+    licensePlateNumber,
+    confidence,
+    camera,
+    timestamp,
+  ) {
+    const endpoint = "log-vehicle/";
+    const pTime = new Date(timestamp).toISOString();
+    const data = {
+      camera: camera,
+      image: encodedImage,
+      results: [
+        {
+          plate: licensePlateNumber,
+          score: confidence,
+          box: { xmin: 0, ymin: 0, ymax: 0, xmax: 0 },
+        },
+      ],
+      time: pTime,
+    };
+    try {
+      let tries = 0;
+      const maxTries = 5;
+      while (tries < maxTries) {
+        const init = {
+          body: JSON.stringify(data),
+          method: "POST",
+          headers: {
+            "content-type": "application/json",
+            Authorization: "Token " + this.token,
+          },
+        };
+        const response = await fetch(this.apiBase + endpoint, init);
+        console.debug("Response: " + response.status);
+        if (response.ok) {
+          console.info("Logged Vehicle");
+          return;
+        } else if (response.status === 429) {
+          tries++;
+          setTimeout(() => {}, 1000);
+        } else {
+          throw new Error("Error logging vehicle");
+        }
+      }
+    } catch (error) {
+      console.error("Error", error);
+    }
+  }
+}
+
+class VerkadaApi {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  static async downloadImage(url) {
+    try {
+      const res = await fetch(url);
+      if (res.ok) {
+        const blob = await res.blob();
+        const reader = new FileReader();
+        reader.readAsDataURL(blob);
+        return new Promise((resolve) => {
+          reader.onloadend = function () {
+            resolve(reader.result.split(",")[1]);
+          };
+        });
+      } else {
+        console.debug("downloadImage res:", res);
+      }
+    } catch (error) {
+      console.error("error", error);
+    }
+  }
+
+  async getSeenLicensePlateImage(cameraId, timestamp, plate) {
+    const params = {
+      page_size: 5,
+      camera_id: cameraId,
+      license_plate: plate,
+      start_time: timestamp - 1,
+      end_time: timestamp + 1,
+    };
+    const url = "https://api.verkada.com/cameras/v1/analytics/lpr/images";
+    const headers = {
+      accept: "application/json",
+      "x-api-key": this.apiKey,
+    };
+    try {
+      const response = await fetch(`${url}?${new URLSearchParams(params)}`, {
+        headers,
+      });
+      if (response.ok) {
+        const data = await response.json();
+        for (const detection of data.detections) {
+          if (
+            detection.timestamp === timestamp &&
+            detection.license_plate === plate
+          ) {
+            return await VerkadaApi.downloadImage(detection.image_url);
+          }
+        }
+      } else {
+        console.error(response.statusText);
+      }
+    } catch (error) {
+      console.error("error", error);
+    }
+  }
+}
+
+export default {
+  // Our fetch handler is invoked on a HTTP request: we can send a message to a queue
+  // during (or after) a request.
+  // https://developers.cloudflare.com/queues/platform/javascript-apis/#producer
+  async fetch(request, env, ctx) {
+    if (request.method === "POST") {
+      const contentType = request.headers.get("content-type");
+      if (contentType.includes("application/json")) {
+        const data = await request.json();
+        const webhookType = data["webhook_type"];
+        if (webhookType !== "lpr") {
+          return new Response(`Unexpected webhook type: ${webhookType}`);
+        }
+        await env.LPR_WEBHOOKS.send(data);
+        return new Response("OK!");
+      } else {
+        return new Response("Error - Required application/json ");
+      }
+    } else {
+      return new Response("Error - Required POST");
+    }
+  },
+
+  // The queue handler is invoked when a batch of messages is ready to be delivered
+  // https://developers.cloudflare.com/queues/platform/javascript-apis/#messagebatch
+  async queue(batch, env) {
+    const verkada = new VerkadaApi(env.VERKADA_API_KEY);
+    const parkpow = new ParkPowApi(env.PARKPOW_TOKEN, env.PARKPOW_URL);
+
+    // A queue consumer can make requests to other endpoints on the Internet,
+    // write to R2 object storage, query a D1 Database, and much more.
+    for (let message of batch.messages) {
+      // Process each message (we'll just log these)
+      const data = message.body["data"];
+      let cameraId = data["camera_id"];
+      let createdAt = data["created"];
+      let confidence = data["confidence"];
+      let licensePlateNumber = data["license_plate_number"];
+
+      let image = verkada.getSeenLicensePlateImage(
+        cameraId,
+        createdAt,
+        licensePlateNumber,
+      );
+      if (image) {
+        await parkpow.logVehicle(
+          image,
+          licensePlateNumber,
+          confidence,
+          cameraId,
+          createdAt,
+        );
+      } else {
+        console.error(
+          `Skip webhook - [${licensePlateNumber}] at ${createdAt} - Missing image`,
+        );
+      }
+    }
+  },
+};
