fe87f558ac7c2b5f7c41842556dd674406165e74
nyaundi brian <danleyb2@gmail.com>
2024-10-29 18:37:45 +0300

    refactor: convert tabs to 4 spaces, remove .editorconfig

--- snapshot-middleware/src/snapshot.js
+++ snapshot-middleware/src/snapshot.js
@@ -1,52 +1,52 @@
 import { fetchWithRetry } from "./utils";
 
 export class SnapshotApi {
-	constructor(token, sdkUrl = null) {
-		if (token === null) {
-			throw new Error("Snapshot API token is required for authentication.");
-		} else {
-			this.token = token;
-		}
-		if (sdkUrl) {
-			this.apiBase = sdkUrl;
-		} else {
-			this.apiBase = "https://api.platerecognizer.com";
-		}
-		console.debug("Api Base: " + this.apiBase);
-	}
+  constructor(token, sdkUrl = null) {
+    if (token === null) {
+      throw new Error("Snapshot API token is required for authentication.");
+    } else {
+      this.token = token;
+    }
+    if (sdkUrl) {
+      this.apiBase = sdkUrl;
+    } else {
+      this.apiBase = "https://api.platerecognizer.com";
+    }
+    console.debug("Api Base: " + this.apiBase);
+  }
 
-	async uploadBase64(encodedImage, camera, timestamp, params) {
-		console.debug(params);
-		const endpoint = "/v1/plate-reader/";
-		const body = new FormData();
-		body.set("upload", encodedImage);
-		body.set("timestamp", timestamp);
+  async uploadBase64(encodedImage, camera, timestamp, params) {
+    console.debug(params);
+    const endpoint = "/v1/plate-reader/";
+    const body = new FormData();
+    body.set("upload", encodedImage);
+    body.set("timestamp", timestamp);
 
-		if (params["camera_id"]) {
-			body.set("camera_id", params["camera_id"]);
-		} else {
-			body.set("camera_id", camera);
-		}
-		if (params["mmc"]) {
-			body.set("mmc", params["mmc"]);
-		} else {
-			body.set("mmc", "true");
-		}
-		if (params["regions"]) {
-			body.set("regions", params["regions"]);
-		}
-		if (params["config"]) {
-			body.set("config", params["config"]);
-		}
+    if (params["camera_id"]) {
+      body.set("camera_id", params["camera_id"]);
+    } else {
+      body.set("camera_id", camera);
+    }
+    if (params["mmc"]) {
+      body.set("mmc", params["mmc"]);
+    } else {
+      body.set("mmc", "true");
+    }
+    if (params["regions"]) {
+      body.set("regions", params["regions"]);
+    }
+    if (params["config"]) {
+      body.set("config", params["config"]);
+    }
 
-		let init = {
-			body: body,
-			method: "POST",
-			headers: {
-				Authorization: "Token " + this.token,
-			},
-		};
-		const url = this.apiBase + endpoint;
-		return fetchWithRetry(url, init).then((response) => response.json());
-	}
+    let init = {
+      body: body,
+      method: "POST",
+      headers: {
+        Authorization: "Token " + this.token,
+      },
+    };
+    const url = this.apiBase + endpoint;
+    return fetchWithRetry(url, init).then((response) => response.json());
+  }
 }
