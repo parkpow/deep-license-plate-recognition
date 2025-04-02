0ce0ae45ca2997714dc90694ca67136a298eefb9
nyaundi brian <danleyb2@gmail.com>
2025-04-02 22:28:38 +0300

    test: snapshot middleware implementation wip

--- snapshot-middleware/test/index.spec.js
+++ snapshot-middleware/test/index.spec.js
@@ -14,14 +14,17 @@ import {
   it,
   vi,
   MockInstance,
+  test,
 } from "vitest";
 
-import worker from "../src";
+import worker from "../src/index";
 
 import GenetecSamplePayload from "./Genetec.json";
 import GenetecSnapshotResponse from "./GenetecSnapshot.json";
 import SurvisionSamplePayload from "./Survision.json";
 import SurvisionSnapshotResponse from "./SurvisionSnapshot.json";
+
+const WORKER_REQUEST_INPUT = "http://snapshot-middleware.platerecognizer.com";
 
 beforeAll(() => {
   // throw errors if an outbound request isn't mocked
@@ -38,6 +41,7 @@ afterEach(() => {
  * Create Expected Request from cameras with relevant headers and payload
  * @param input
  * @param jsonData
+ * @param extraHeaders
  * @returns {Request}
  */
 function createJsonUploadRequest(input, jsonData, extraHeaders = {}) {
@@ -53,109 +57,97 @@ function createJsonUploadRequest(input, jsonData, extraHeaders = {}) {
   });
 }
 
-// describe("Snapshot Upload", () => {
-it("GET Response Error - Required POST", async () => {
-  const request = new Request("http://snapshot-middleware.platerecognizer.com");
-  // Create an empty context to pass to `worker.fetch()`
-  const ctx = createExecutionContext();
-  const response = await worker.fetch(request, env, ctx);
+describe("Snapshot Upload", () => {
+  it("GET Response Error - Required POST", async () => {
+    const request = new Request(WORKER_REQUEST_INPUT);
+    // Create an empty context to pass to `worker.fetch()`
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
 
-  // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-  await waitOnExecutionContext(ctx);
-  expect(await response.status).toBe(400);
-  expect(await response.text()).toBe("Error - Required POST");
-});
+    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+    await waitOnExecutionContext(ctx);
+    expect(await response.status).toBe(400);
+    expect(await response.text()).toBe("Error - Required POST");
+  });
 
-it("No Content-Length Headers - Expected Content-Type application/json and Content-Length > 0", async () => {
-  const request = new Request(
-    "http://snapshot-middleware.platerecognizer.com",
-    {
+  it("No Content-Length Headers - Expected Content-Type application/json and Content-Length > 0", async () => {
+    const request = new Request(WORKER_REQUEST_INPUT, {
       method: "POST",
-    },
-  );
-  // Create an empty context to pass to `worker.fetch()`
-  const ctx = createExecutionContext();
-  const response = await worker.fetch(request, env, ctx);
-  // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-  await waitOnExecutionContext(ctx);
-  expect(await response.status).toBe(400);
-  expect(await response.text()).toBe(
-    "Error - Expected Content-Type application/json and Content-Length > 0",
-  );
-});
+    });
+    // Create an empty context to pass to `worker.fetch()`
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+    await waitOnExecutionContext(ctx);
+    expect(await response.status).toBe(400);
+    expect(await response.text()).toBe(
+      "Error - Expected Content-Type application/json and Content-Length > 0",
+    );
+  });
 
-it("No Expected Content-Type - Expected Content-Type application/json and Content-Length > 0", async () => {
-  const request = new Request(
-    "http://snapshot-middleware.platerecognizer.com",
-    {
+  it("No Expected Content-Type - Expected Content-Type application/json and Content-Length > 0", async () => {
+    const request = new Request(WORKER_REQUEST_INPUT, {
       method: "POST",
       headers: {
         "content-Length": 10,
         "content-Type": "application/not-json",
       },
-    },
-  );
-  // Create an empty context to pass to `worker.fetch()`
-  const ctx = createExecutionContext();
-  const response = await worker.fetch(request, env, ctx);
-  // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-  await waitOnExecutionContext(ctx);
-  expect(await response.status).toBe(400);
-  expect(await response.text()).toBe(
-    "Error - Expected Content-Type application/json and Content-Length > 0",
-  );
-});
+    });
+    // Create an empty context to pass to `worker.fetch()`
+    const ctx = createExecutionContext();
+    const response = await worker.fetch(request, env, ctx);
+    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+    await waitOnExecutionContext(ctx);
+    expect(await response.status).toBe(400);
+    expect(await response.text()).toBe(
+      "Error - Expected Content-Type application/json and Content-Length > 0",
+    );
+  });
 
-it("Uploads Images In Request to Snapshot", async () => {
-  fetchMock
-    .get("https://api.platerecognizer.com")
-    .intercept({ path: "/v1/plate-reader/", method: "POST" })
-    .reply(200, "body");
-
-  // Test Sample Payloads
-  const payloads = [
-    [GenetecSamplePayload, {}, GenetecSnapshotResponse],
+  test.each([
+    ["genetec", GenetecSamplePayload, {}, GenetecSnapshotResponse],
     [
+      "survision",
       SurvisionSamplePayload,
       { "survision-serial-number": "sv1-searial-1" },
       SurvisionSnapshotResponse,
     ],
-  ];
-  for (let i = 0; i < payloads.length; i++) {
-    let [payload, headers, snapshotResponse] = payloads[i];
-    let request = createJsonUploadRequest(
-      "http://snapshot-middleware.platerecognizer.com",
-      payload,
-      headers,
-    );
-    // Create an empty context to pass to `worker.fetch()`
-    let ctx = createExecutionContext();
-    let response = await worker.fetch(request, env, ctx);
-    // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
-    await waitOnExecutionContext(ctx);
-    expect(await response.status).toBe(200);
-    expect(await response.text()).toBe("Success");
+  ])(
+    "Uploads Images In Request to Snapshot: - %s - ",
+    async (camera, payload, headers, mockSnapshotResponse) => {
+      fetchMock
+        .get("https://api.platerecognizer.com")
+        .intercept({ path: "/v1/plate-reader/", method: "POST" })
+        .reply(200, mockSnapshotResponse);
 
-    // TODO by default the response should be Snapshot response
-    //  unless manually forwarded to ParkPow then it's ParkPow response
-    expect(await snapshotResponse).toBe("Success");
-  }
+      let req = createJsonUploadRequest(WORKER_REQUEST_INPUT, payload, headers);
+      // Create an empty context to pass to `worker.fetch()`
+      let ctx = createExecutionContext();
+      let response = await worker.fetch(req, env, ctx);
+      // Wait for all `Promise`s passed to `ctx.waitUntil()` to settle before running test assertions
+      await waitOnExecutionContext(ctx);
+      expect(await response.status).toBe(200);
+      expect(await response.text()).toBe("Success");
+
+      // TODO by default the response should be Snapshot response
+      //  unless manually forwarded to ParkPow then it's ParkPow response
+      expect(await response.text()).toBe(mockSnapshotResponse);
+    },
+  );
+
+  it("Retries Rate Limits", async () => {
+    // Fake Timers https://github.com/cloudflare/workers-sdk/blob/main/fixtures/vitest-pool-workers-examples/misc/test/fake-timers.test.ts
+    throw new Error("Not Implemented");
+  });
+
+  it("Forwards Back Response", async () => {
+    throw new Error("Not Implemented");
+  });
+
+  it("Invalid Request has Error Response", async () => {
+    throw new Error("Not Implemented");
+  });
 });
-
-it("Retries Rate Limits", async () => {
-  // Fake Timers https://github.com/cloudflare/workers-sdk/blob/main/fixtures/vitest-pool-workers-examples/misc/test/fake-timers.test.ts
-  throw new Error("Not Implemented");
-});
-
-it("Forwards Back Response", async () => {
-  throw new Error("Not Implemented");
-});
-
-it("Invalid Request has Error Response", async () => {
-  throw new Error("Not Implemented");
-});
-
-// });
 
 describe("ParkPow Upload", () => {
   /**
--- snapshot-middleware/test/rollbar.spec.js
+++ snapshot-middleware/test/rollbar.spec.js
@@ -1,13 +1,28 @@
-// import {
-//   env,
-//   createExecutionContext,
-//   waitOnExecutionContext,
-//   SELF,
-// } from "cloudflare:test";
-import { describe, it, expect } from "vitest";
+import {
+  env,
+  createExecutionContext,
+  waitOnExecutionContext,
+  fetchMock,
+} from "cloudflare:test";
+import { describe, it, expect, beforeAll, beforeEach, afterEach } from "vitest";
+import worker from "../src/rollbar";
+
+beforeAll(() => {
+  // throw errors if an outbound request isn't mocked
+  fetchMock.disableNetConnect();
+});
+beforeEach(() => fetchMock.activate());
+afterEach(() => {
+  fetchMock.assertNoPendingInterceptors();
+  fetchMock.deactivate();
+});
 
 describe("Error Logging to RollBar", () => {
   it("Exception is captured", async () => {
-    throw new Error("Not Implemented");
+    const events = [];
+    const ctx = createExecutionContext();
+    const response = await worker.tail(events, env, ctx);
+    await waitOnExecutionContext(ctx);
+    expect(response).toBe(400);
   });
 });
