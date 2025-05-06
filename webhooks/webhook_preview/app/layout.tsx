74087ad963273032f0d69d62aa210fbb09218da6
Talles Tayson <tallestayson@gmail.com>
2025-05-05 22:48:35 -0300

    force light theme
    I changed it to force the light theme in the ThemeProvider; the code is not yet prepared to handle all situations for a dark theme.

--- webhooks/webhook_preview/app/layout.tsx
+++ webhooks/webhook_preview/app/layout.tsx
@@ -21,7 +21,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+        <ThemeProvider attribute="class" defaultTheme="light" enableSystem>
           {children}
           <Toaster />
         </ThemeProvider>
