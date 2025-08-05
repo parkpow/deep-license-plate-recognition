48e133f01b27f83988691752540075f77ad1725d
adolfoarmas <adolfoarmas.90@gmail.com>
2025-08-05 18:37:44 -0300

    adding logging

--- stream/remove_images.py
+++ stream/remove_images.py
@@ -1,8 +1,16 @@
 import argparse
+import logging
 import os
+import sys
 import shutil
 from datetime import datetime, timedelta
 from pathlib import Path
+
+
+logging.basicConfig(
+    stream=sys.stdout,
+    format="%(asctime)s %(levelname)s %(name)s %(threadName)s : %(message)s",
+)
 
 def parse_arguments():
     parser = argparse.ArgumentParser(
@@ -63,7 +71,7 @@ def get_percentages(directory):
 def delete_image(file_path):
     file_size = file_path.stat().st_size
     file_path.unlink()
-    print(f"Deleted: {file_path} ({file_size} bytes)")
+    logging.info(f"Deleted: {file_path} ({file_size} bytes)")
     
 def delete_images_by_percentage(directory, target_free_percent, batch_size=10):
     directory_path = Path(directory)
@@ -73,25 +81,25 @@ def delete_images_by_percentage(directory, target_free_percent, batch_size=10):
     )
 
     _, current_free_percent = get_percentages(directory)
-    print(f"Current free space: {current_free_percent:.2f}%")
+    logging.info(f"Current free space: {current_free_percent:.2f}%")
 
     files_deleted = 0
     
     for file_path in all_files:
         if current_free_percent >= target_free_percent:
-            print(f"Reached target free space: {current_free_percent:.2f}%")
+            logging.info(f"Reached target free space: {current_free_percent:.2f}%")
             break
         try:
             delete_image(file_path)
             files_deleted += 1
         except OSError as e:
-            print(f"Error deleting file {file_path}: {e.strerror}")
+            logging.info(f"Error deleting file {file_path}: {e.strerror}")
 
         if files_deleted % batch_size == 0:
             _, current_free_percent = get_percentages(directory)
             
     _, current_free_percent = get_percentages(directory)
-    print(f"Final free space after deletion: {current_free_percent:.2f}%")
+    logging.info(f"Final free space after deletion: {current_free_percent:.2f}%")
 
 
 # Function to delete image files
@@ -107,11 +115,11 @@ def delete_images_by_age(directory, threshold_minutes):
                 delete_image(file_path)
                 files_deleted += 1
             except OSError as e:
-                print(f"Error deleting images: {e.filename} - {e.strerror}")
+                logging.info(f"Error deleting images: {e.filename} - {e.strerror}")
     
     if files_deleted == 0:
-        print(f"No file(s) to delete below the threshold time")
-        
+        logging.info(f"No file(s) to delete below the threshold time")
+
 # Function to delete empty directories
 def delete_empty_directories(directory):
     for dir_path in reversed(list(Path(directory).rglob("*"))):
@@ -120,7 +128,7 @@ def delete_empty_directories(directory):
                 if not list(dir_path.iterdir()):
                     dir_path.rmdir()
             except OSError as e:
-                print(f"Error deleting empty directories: {e.filename} - {e.strerror}")
+                logging.info(f"Error deleting empty directories: {e.filename} - {e.strerror}")
 
 
 def main():
@@ -129,20 +137,20 @@ def main():
 
     if args.usage:
         if args.target_free is None or args.trigger_usage is None:
-            print("Error: --target_free and --trigger_usage are required in --usage mode.")
+            logging.error("Error: --target_free and --trigger_usage are required in --usage mode.")
             return
         current_used_percent, _ = get_percentages(directory)
-        print(f"Disk usage: {current_used_percent:.2f}%")
+        logging.info(f"Disk usage: {current_used_percent:.2f}%")
         if current_used_percent >= args.trigger_usage:
-            print(f"Disk usage exceeded {args.trigger_usage}%. Starting cleanup...")
+            logging.info(f"Disk usage exceeded {args.trigger_usage}%. Starting cleanup...")
             delete_images_by_percentage(directory, args.target_free)
             delete_empty_directories(directory)
         else:
-            print(f"Disk usage is below {args.trigger_usage}%. No cleanup needed.")
+            logging.info(f"Disk usage is below {args.trigger_usage}%. No cleanup needed.")
 
     elif args.age:
         if args.threshold is None:
-            print("Error: --threshold is required in --age mode.")
+            logging.error("Error: --threshold is required in --age mode.")
             return
         threshold_minutes = args.threshold * 60
         delete_images_by_age(directory, threshold_minutes)
